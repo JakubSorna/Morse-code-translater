48297dc3891ff2a1d9702f7979907f017ff1949a
Jakub Šorna <sornajakub@gmail.com>
2022-09-07 01:28:22 +0200

    word to morse finished

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ morse_to_letter = inverse_morse_code()
 def choose():
     choice = input("Insert 'a' to convert letter to morse code and 'm' morse code to letters.")
     return choice
+
+def Input():
+    word = input("Insert word:")
+    word = word.lower()
+    return word
+def word_to_morse():
+    word = Input()
+    morse = []
+    for letter in word:
+        morse.append(letter_to_morse[letter])
+    print(" ".join(morse))
+
+
+word_to_morse()
